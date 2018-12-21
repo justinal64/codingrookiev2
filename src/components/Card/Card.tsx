58ccba163bbfcff18f198b0c1dc8e5ce00f31a25
justin leggett <justin@codingrookie.com>
2018-12-20 22:24:53 -0600

    removed styled components that aren't being used and commented out props to fix a build error

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,29 +1,6 @@
 import * as React from 'react'
 import styled from 'react-emotion'
 
-const StyledNavigation = styled.div`
-  display: flex;
-  /* min-height: 10vh; */
-  background-color: pink;
-  display: flex;
-  justify-content: space-between;
-`
-const StyledNavigationLogo = styled.div`
-  display: flex;
-`
-
-const StyledNavigationDiv = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`
-
-const StyledNavigationLi = styled.li`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  padding: 1em;
-`
-
 const CardContainer = styled.div`
   display: flex;
   flex-direction: row;
