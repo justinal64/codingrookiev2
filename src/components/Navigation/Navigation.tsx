23d921238dfe9a32e40eb6c3d21e3d59c9f103d0
justin leggett <justin@codingrookie.com>
2018-12-29 13:59:05 -0600

    added side navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,24 +3,14 @@ import styled from 'react-emotion'
 
 const StyledNavigation = styled.div`
   display: flex;
+  flex-direction: column;
   /* min-height: 10vh; */
   background-color: pink;
-  display: flex;
-  justify-content: space-between;
-`
-const StyledNavigationLogo = styled.div`
-  display: flex;
-`
-
-const StyledNavigationDiv = styled.div`
-  display: flex;
-  justify-content: flex-end;
+  /* justify-content: space-between; */
 `
 
 const StyledNavigationLi = styled.li`
   display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
   padding: 1em;
 `
 
@@ -31,17 +21,13 @@ interface NavigationRootProps {
 
 const Navigation: React.SFC<NavigationRootProps> = ({ className, title }) => (
   <StyledNavigation className={className}>
-    <StyledNavigationLogo>
-      <StyledNavigationLi>{title}</StyledNavigationLi>
-    </StyledNavigationLogo>
-    <StyledNavigationDiv>
-      {/* refactor to forEach or map? */}
-      <StyledNavigationLi>Tutorials</StyledNavigationLi>
-      <StyledNavigationLi>Blog</StyledNavigationLi>
-      <StyledNavigationLi>Store</StyledNavigationLi>
-      <StyledNavigationLi>Login</StyledNavigationLi>
-      <StyledNavigationLi>Sign Up</StyledNavigationLi>
-    </StyledNavigationDiv>
+    <StyledNavigationLi>{title} Logo Here!!</StyledNavigationLi>
+    {/* refactor to forEach or map? */}
+    <StyledNavigationLi>Tutorials</StyledNavigationLi>
+    <StyledNavigationLi>Blog</StyledNavigationLi>
+    <StyledNavigationLi>Store</StyledNavigationLi>
+    <StyledNavigationLi>Login</StyledNavigationLi>
+    <StyledNavigationLi>Sign Up</StyledNavigationLi>
   </StyledNavigation>
 )
 
